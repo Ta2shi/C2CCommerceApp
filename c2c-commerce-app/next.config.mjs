51b63ebbbed6e447c74f25305feed78f89be8ae7
Ta2shi <20121048@kaishi-pu.ac.jp>
2024-02-22 01:27:09 +0900

    ESLintの設定

--- c2c-commerce-app/next.config.mjs
+++ c2c-commerce-app/next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  compiler: {
+    styledComponents: true, // styledComponentsの有効化
+  },
 };
 
 export default nextConfig;
