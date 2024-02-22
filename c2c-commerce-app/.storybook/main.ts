e9a6f20f0755627216f2ff349789fffaab68e93a
Ta2shi <20121048@kaishi-pu.ac.jp>
2024-02-22 17:32:57 +0900

    ライブラリのインストール、envファイルの追加

--- c2c-commerce-app/.storybook/main.ts
+++ c2c-commerce-app/.storybook/main.ts
@@ -1,20 +1,53 @@
-import type { StorybookConfig } from "@storybook/nextjs";
+import TsconfigPathsPlugin from 'tsconfig-paths-webpack-plugin';
+const path = require('path')
 
-const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-onboarding",
-    "@storybook/addon-interactions",
+module.exports = {
+  stories: [
+    '../src/**/*.stories.mdx',
+    '../src/**/*.stories.@(js|jsx|ts|tsx)'
   ],
-  framework: {
-    name: "@storybook/nextjs",
-    options: {},
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-postcss'
+  ],
+  staticDirs: ['publlic'],
+  bable: async options => ({
+    ...options,
+    plugin: [
+      '@babel/plugin-proposal-class-properties',
+      '@babel/plugin-proposal-private-methods',
+      '@babel/plugin-proposal-private-property-in-object'
+    ],
+  }),
+  webpackFinal: async (config) => {
+    config.resolve.plugins = [
+      new TsconfigPathsPlugin({
+        configFile: path.resolve(__dirname, '../tsconfig.json')
+      }),
+    ],
+    config
   },
-  docs: {
-    autodocs: "tag",
-  },
-  staticDirs: ['public']
-};
-export default config;
+}
+
+// **initial settings**
+// import type { StorybookConfig } from "@storybook/nextjs";
+
+// const config: StorybookConfig = {
+//   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+//   addons: [
+//     "@storybook/addon-links",
+//     "@storybook/addon-essentials",
+//     "@storybook/addon-onboarding",
+//     "@storybook/addon-interactions",
+//   ],
+//   framework: {
+//     name: "@storybook/nextjs",
+//     options: {},
+//   },
+//   docs: {
+//     autodocs: "tag",
+//   },
+//   staticDirs: ['public']
+// };
+// export default config;
--- c2c-commerce-app/.storybook/preview.ts
+++ c2c-commerce-app/.storybook/preview.ts
@@ -1,15 +1,72 @@
-import type { Preview } from "@storybook/react";
+import { Decorator } from '@storybook/react';
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
+import { theme } from '../src/themes';
+import * as NextImage from 'next/image';
 
-const preview: Preview = {
-  parameters: {
-    actions: { argTypesRegex: "^on[A-Z].*" },
-    controls: {
-      matchers: {
-        color: /(background|color)$/i,
-        date: /Date$/i,
-      },
+export const parameters = {
+  actions: { argTypesRegex: '^on[A-Z].*' },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
     },
   },
 };
 
-export default preview;
+export const GlobalStyle = createGlobalStyle`
+  html,
+  body,
+  textarea {
+    padding: 0;
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+  }
+  * {
+    box-sizing: border-box;
+  }
+  a {
+    text-decoration: none;
+    transition: .25s;
+    color: #000000;
+  }
+`;
+
+// // Themeの適用
+// const withThemeProvider: Decorator<JSX.Element> = (story) => (
+//   <ThemeProvider theme={theme}>
+//     <GlobalStyle />
+//     {story()}
+//   </ThemeProvider>
+// );
+
+// export const decorators = [withThemeProvider];
+
+// // next/imageの差し替え
+// const OriginalNextImage = NextImage.default as any;
+
+// Object.defineProperty(NextImage, 'default', {
+//   configurable: true,
+//   value: (props: any) =>
+//     typeof props.src === 'string' ? (
+//       <OriginalNextImage {...props} unoptimized blurDataURL={props.src} />
+//     ) : (
+//       <OriginalNextImage {...props} unoptimized />
+//     ),
+// });
+
+// **initial settings**
+// import type { Preview } from "@storybook/react";
+
+// const preview: Preview = {
+//   parameters: {
+//     actions: { argTypesRegex: "^on[A-Z].*" },
+//     controls: {
+//       matchers: {
+//         color: /(background|color)$/i,
+//         date: /Date$/i,
+//       },
+//     },
+//   },
+// };
+
+// export default preview;
