c8193b5e6024b38a0f4a041d391f6237e0660f38
Ta2shi <20121048@kaishi-pu.ac.jp>
2024-02-22 18:10:01 +0900

    next.configの設定

--- c2c-commerce-app/next.config.mjs
+++ c2c-commerce-app/next.config.mjs
@@ -1,9 +1,32 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  compiler: {
-    styledComponents: true, // styledComponentsの有効化
-  },
-};
+  compiler: (() => {
+    let compilerConfig = {
+      // styledComponentsの有効化
+      styledComponents: true,
+    }
 
-export default nextConfig;
+    if(process.env.NODE_ENV === 'production') {
+      compilerConfig = {
+        ...compilerConfig,
+        // 本番環境ではReact Testing Libraryで使用するdata-testid属性を削除
+        reactRemoveProperties: {properties: ['^data-testid$'] },
+      }
+    }
+    return compilerConfig
+  }) (),
+}
+
+module.exports = nextConfig
+
+// **initial setting**
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+//   compiler: {
+//     styledComponents: true, // styledComponentsの有効化
+//   },
+// };
+
+// export default nextConfig;
