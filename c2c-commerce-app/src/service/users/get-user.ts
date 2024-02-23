f01ac914e0fbf01926f271c7378ac8a3a2644ade
Ta2shi <20121048@kaishi-pu.ac.jp>
2024-02-23 18:53:47 +0900

    APIクライアントの実装

---
+++ c2c-commerce-app/src/service/users/get-user.ts
@@ -0,0 +1,36 @@
+import type { ApiContext, User } from 'types/data'
+import { fetcher } from 'utils'
+
+export type GetUserParmas = {
+    /** ユーザーID */
+    id: number
+}
+
+const getUser = async (
+    context: ApiContext,
+    { id }: GetUserParmas,
+) : Promise<User> => {
+    /**
+    // ユーザAPI
+    // サンプルレスポンス
+    {
+        "id": "1",
+        "username": "taketo",
+        "displayName": "Taketo Yoshida",
+        "email": "taketo@example,com",
+        "profileImageUrl": "/user/1.png",
+        "description": "Lorem Ipsum is simply dummy text of the printing and typesetting industry"
+    }
+     */
+    return await fetcher(
+        `${context.apiRootUrl.replace(/\/$/g, '')}/users/${id}`,
+        {
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+            },
+        },
+    )
+}
+
+export default getUser
